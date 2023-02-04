c684451976c19df7b093ace0d89494602af3475b
Shokha Way <87269516+shokhaway01@users.noreply.github.com>
2023-02-05 00:42:24 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let signInBtn = document.querySelector("#sign-in-btn")
 let signUpBtn = document.querySelector('#sign-up-btn');
 let container = document.querySelector(".container");
 
+let signInBtn2 = document.querySelector("#sign-in-btn2")
+let signUpBtn2 = document.querySelector('#sign-up-btn2');
+
 signUpBtn.addEventListener('click', () => {
     container.classList.add("sign-up-mode")
 })
